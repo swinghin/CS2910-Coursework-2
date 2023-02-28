81cc89048e53d10a36b90126b927f7081142f8aa
Wing Hin Shih <wing.shih.2020@live.rhul.ac.uk>
2023-02-28 19:16:36 +0000

    Check if orig and dest valid

--- cs2910CW2.pl
+++ cs2910CW2.pl
@@ -1,16 +1,6 @@
 % 3.1 Finding a path
 
-location(outside).
-location(porch1).
-location(porch2).
-location(livingroom).
-location(kitchen).
-location(corridor).
-location(wc).
-location(bedroom).
-location(masterbedroom).
-
-% doorway definitions for the two bedroom house
+% doorway/2 definitions for the two bedroom house
 doorway(livingroom,kitchen).
 doorway(kitchen,porch1).
 doorway(porch1,outside).
@@ -26,16 +16,36 @@ doorway(porch2,outside).
 % connected/2 in terms of doorway and reverse
 connected(Location1,Location2) :- doorway(Location1,Location2) ; doorway(Location2,Location1).
 
-% path/3 definition
+% path/3 definition, returns path from origin to destination if exists
 path(Origin,Destination,Path) :-
-    find(Origin,Destination,[Origin],Way),
-    reverse(Way,Path).
+    orig_valid(Origin), % check if origin is valid
+    dest_valid(Destination), % check if destination is valid
+    find(Origin,Destination,[Origin],Way), % recursive call to find path
+    reverse(Way,Path). % reverse backtracking stack
 
 % find/4 backtracking search
-find(Origin,Destination,Path,[Destination|Path]) :-
-    connected(Origin,Destination).
-find(Origin,Destination,Visited,Way) :-
-    connected(Origin,Midpoint),
-    Midpoint \== Destination,
+find(Location1,Location2,Path,[Location2|Path]) :-
+    connected(Location1,Location2).
+find(Location1,Location2,Visited,Way) :-
+    connected(Location1,Midpoint),
+    Midpoint \== Location2,
     \+ member(Midpoint,Visited),
-    find(Midpoint,Destination,[Midpoint|Visited],Way).
+    find(Midpoint,Location2,[Midpoint|Visited],Way).
+
+% locations/1 defines a list of valid locations
+locations([outside,porch1,porch2,livingroom,kitchen,corridor,wc,bedroom,masterbedroom]).
+
+% location_valid/1 returns if location exists in locations list
+location_valid(Location) :-
+    locations(Locations), % loads locations list
+    member(Location, Locations), !. % check variable is member of list
+location_valid(_) :- fail. % return false if not valid
+
+orig_valid(Location) :- location_valid(Location), !.
+orig_valid(Location) :- \+ location_valid(Location),
+    format('~w not a valid origin.', Location), fail.
+
+dest_valid(Location) :- location_valid(Location), !.
+dest_valid(Location) :- \+ location_valid(Location),
+    format('~w not a valid destination.', Location), fail.
+    
